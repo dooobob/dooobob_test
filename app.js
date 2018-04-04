d3fbd32e0da34dbdc82ffaa93c89a92449893ac9
김경태 <kimtae2129@gmai.com>
2018-04-04 16:49:00 +0900

    correct API 'healthCheck'

--- app.js
+++ app.js
@@ -25,15 +25,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 
+app.use('/healthCheck', function(req, res) {
+    res.send('healthCheck');
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
-});
-
-app.use('/healthCheck', function(req, res) {
-    res.send('healthCheck');
 });
 
 // error handlers
